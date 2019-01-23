a610b89bdd3fefa1d0b5202e08857c0a726969bb
Benjamin Crozat <benjamincrozat@me.com>
2019-01-23 12:11:14 +0100

    Annoted the Facade

--- src/Facades/Polyglot.php
+++ src/Facades/Polyglot.php
@@ -4,6 +4,11 @@ namespace BC\Laravel\Settings\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string urlToLanguage(string $language)
+ * @method static string prefix()
+ * @method static void setLocale()
+ */
 class Polyglot extends Facade
 {
     protected static function getFacadeAccessor()

2cd111593c49b081de955b63ecaeb9277da32d89
Benjamin Crozat <benjamincrozat@me.com>
2019-01-23 11:54:37 +0100

    Documented the config file

--- config/polyglot.php
+++ config/polyglot.php
@@ -1,10 +1,39 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Polyglot Driver
+    |--------------------------------------------------------------------------
+    |
+    | The technique used to differenciate and switch languages is
+    | defined by a driver. Choose the one that fits your needs.
+    |
+    | Supported: "directories", "domains"
+    |
+    */
     'driver' => 'directories',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Supported Languages
+    |--------------------------------------------------------------------------
+    |
+    | This parameter is useful when using the `directories` driver, since
+    | it prevents users to display unsupported or imcomplete languages.
+    |
+    */
     'languages' => ['en'],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Supported Languages
+    |--------------------------------------------------------------------------
+    |
+    | When using the `domains` driver, this is where you associate
+    | a language to a domain. It can be a full or a sub domain.
+    |
+    */
     'domains' => [
         'en' => env('POLYGLOT_EN_DOMAIN'),
     ],

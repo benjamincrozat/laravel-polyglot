2896cf2956ac4a6d6b203c4668bfedf108350a65
Benjamin Crozat <benjamincrozat@me.com>
2019-07-23 16:41:04 +0200

    WIP

--- src/Drivers/QueryStringDriver.php
+++ src/Drivers/QueryStringDriver.php
@@ -3,6 +3,7 @@
 namespace BC\Laravel\Polyglot\Drivers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class QueryStringDriver implements DriverContract
 {
@@ -29,7 +30,9 @@ class QueryStringDriver implements DriverContract
      */
     public function setLocale()
     {
-        app()->setLocale($this->request->language ?? config('app.locale'));
+        app()->setLocale(
+            $this->preferredLocale()
+        );
     }
 
     /**
@@ -38,5 +41,33 @@ class QueryStringDriver implements DriverContract
     public function switchTo($language)
     {
         return $this->request->fullUrlWithQuery(compact('language'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function preferredLocale()
+    {
+        if ($this->request->language && $this->isValidLanguage($this->request->language)) {
+            Cookie::queue('language', $this->request->language, 60 * 24 * 365);
+
+            return $this->request->language;
+        }
+
+        if (! empty($this->request->cookie('language')) && $this->isValidLanguage($language = $this->request->cookie('language'))) {
+            return $language;
+        }
+
+        return explode('_', $this->request->getPreferredLanguage())[0];
+    }
+
+    /**
+     * @param string $language
+     *
+     * @return bool
+     */
+    protected function isValidLanguage($language)
+    {
+        return in_array($language, config('polyglot.languages'));
     }
 }

30fbe6971c2b607adea49e1d18df486215b4f964
Benjamin Crozat <benjamincrozat@me.com>
2020-03-01 16:16:51 +0100

    WIP

--- resources/views/links.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach (config('polyglot.languages', ['en' => 'English']) as $code => $name)
-    <link rel="alternate" hreflang="{{ $code }}" href="{{ polyglot()->switchTo($code) }}">
-@endforeach
--- src/Polyglot.php
+++ src/Polyglot.php
@@ -2,15 +2,20 @@
 
 namespace BC\Laravel\Polyglot;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use BC\Laravel\Polyglot\Drivers\DirectoriesDriver;
 
 class Polyglot
 {
     public DirectoriesDriver $driver;
 
-    public function __construct(DirectoriesDriver $driver)
+    public Request $request;
+
+    public function __construct(DirectoriesDriver $driver, Request $request)
     {
-        $this->driver = $driver;
+        $this->driver  = $driver;
+        $this->request = $request;
     }
 
     public function __call($name, $arguments)
@@ -18,6 +23,31 @@ class Polyglot
         if (method_exists($this->driver(), $name)) {
             return $this->driver()->{$name}(...$arguments);
         }
+    }
+
+    public function route($name, $parameters = [], $absolute = true) : string
+    {
+        return app('url')->route(app()->getLocale() . ".$name", $parameters, $absolute);
+    }
+
+    public function routeIs(...$patterns) : string
+    {
+        $patterns[0] = app()->getLocale() . '.' . $patterns[0];
+
+        return Route::is($patterns);
+    }
+
+    public function switchTo(string $language) : string
+    {
+        if ($directory = $this->request->segments()[0] ?? '') {
+            return preg_replace("/\/$directory/", "/$language", $this->request->fullUrl());
+        }
+
+        if ($this->request->getQueryString()) {
+            return "/$language{$this->request->path()}?{$this->request->getQueryString()}";
+        }
+
+        return "/$language{$this->request->path()}";
     }
 
     public function driver() : DirectoriesDriver

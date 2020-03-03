fa2382143b66743c1437ac3b63bbfb8b058a3291
Benjamin Crozat <benjamincrozat@me.com>
2020-03-03 17:02:32 +0100

    Fixed Laravel 7 breaking change

--- src/Middlewares/Localized.php
+++ src/Middlewares/Localized.php
@@ -10,14 +10,15 @@ class Localized
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @param Closure                  $next
      * @param string|null              $guard
      *
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        app()->setLocale($request->route()->getPrefix());
+        app()->setLocale(
+            trim($request->route()->getPrefix(), '/')
+        );
 
         return $next($request);
     }

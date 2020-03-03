366d434f5910810b69771817a02c609378acb92b
Benjamin Crozat <benjamincrozat@me.com>
2020-03-03 17:16:01 +0100

    Removed doc block

--- src/Facades/Polyglot.php
+++ src/Facades/Polyglot.php
@@ -4,12 +4,6 @@ namespace BC\Laravel\Polyglot\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static string prefix()
- * @method static void setLocale()
- * @method static string switchTo(string $language)
- * @method static \BC\Laravel\Polyglot\Drivers\DirectoriesDriver driver()
- */
 class Polyglot extends Facade
 {
     protected static function getFacadeAccessor()
